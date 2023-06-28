1ae623292bdd1dfc632f8b7670f585b0617e8bf4
LZTD1 <danil227pavlov@gmail.com>
2023-06-28 16:35:45 +0300

    Установление связей в сущностях Многие ко многим

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,6 +5,6 @@ namespace PokemonReviewApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
     }
 }
