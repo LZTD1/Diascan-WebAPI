4d542614021efb8b5ded3706612589e5338c011f
LZTD1 <danil227pavlov@gmail.com>
2023-06-28 15:50:58 +0300

    Установление связей в сущностях Один к одному, один ко многим

--- Models/Review.cs
+++ Models/Review.cs
@@ -5,5 +5,7 @@
         public int id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
+        public Reviewer Reviewer { get; set; }
+        public Pokemon Pokemon { get; set; }
     }
 }
